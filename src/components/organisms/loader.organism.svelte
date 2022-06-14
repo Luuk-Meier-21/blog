85cb2c854de6e075ca194208ab1add21abd91a79
Luuk-Meier-21 <luuk.meier@outlook.com>
2022-06-14 23:44:14 +0200

    added queries for graphql

--- src/components/organisms/loader.organism.svelte
+++ src/components/organisms/loader.organism.svelte
@@ -12,20 +12,27 @@
 
 <script lang="ts">
     import { setContext } from "svelte";
+    import { gqlFetch, gqlQueryResolver } from "../../utilities/queries";
     import type { Tag } from "../molecules/tags.molecule.svelte";
     import type { PostData } from "./post.organism.svelte";
+
 	export let ref: ApiRef = {
-		url: "https://api-eu-west-2.graphcms.com/v2/cl3ossjir6ju301z6grqwfdf9/master?query=query%20MyQuery%20%7B%0A%20%20tags%20%7B%0A%20%20%20%20label%0A%20%20%20%20url%0A%20%20%20%20blogPosts%20%7B%0A%20%20%20%20%20%20slug%0A%20%20%20%20%20%20title%0A%20%20%20%20%20%20tags%20%7B%0A%20%20%20%20%20%20%20%20label%0A%20%20%20%20%20%20%20%20url%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20content%20%7B%0A%20%20%20%20%20%20%20%20html%0A%20%20%20%20%20%20%20%20text%0A%20%20%20%20%20%20%7D%0A%20%20%09%7D%0A%20%20%7D%0A%20%20blogPosts%20%7B%0A%20%20%20%20slug%0A%20%20%20%20title%0A%20%20%20%20tags%20%7B%0A%20%20%20%20%20%20label%0A%20%20%20%20%20%20url%0A%20%20%20%20%7D%0A%20%20%20%20content%20%7B%0A%20%20%20%20%20%20html%0A%20%20%20%20%20%20text%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D&operationName=MyQuery",
+		url: "https://api-eu-west-2.graphcms.com/v2/cl3ossjir6ju301z6grqwfdf9/master",
 		queryName: "api"
 	}
+
     let data: ApiData = {
         tags: [],
         blogPosts: []
     };
 
 	const fetchPosts = (async () => {
-        const response = await fetch(ref.url);
+        const response = await gqlFetch((q) => [
+            q.tags,
+            q.blogPosts
+        ])
         const json = await response.json();
+        console.log(json)
         data = json.data;
 	})();
 
